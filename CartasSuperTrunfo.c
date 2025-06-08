6574b9091e18bd6ccab0a7560f23e21f533ba742
Jose Torres <78854535+Torres1616@users.noreply.github.com>
2025-06-08 03:15:29 +0000

    Carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 
+//Criação das variaveis.
+
 int main() {
     char estado;
     char codigocarta[20];
     char cidade[20];
-    int populacão;
+    float populacao;
     float area;
     float pib;
     int turistico;
+
+//Criação da entrada de dados
+//O ser irá preencher os dados com suas devidas informações, referentes aos seus campos.
+//Printf: Determina o que está sendo pedido, de deverá ser preenchido no terminal;
+//Scanf: Determina a entrada dos dados e o seu tipo.
     
+    printf("Carta: 1\n");
     printf("Insira a letra inicial do seu estado: ");
     scanf("%c", &estado);
 
     printf("Insira a inicial do estado junto de um número de 01 á 04: ");
-    scanf("%s", &codigocarta);
+    scanf("%s", &codigocarta);//(EX. C01 ).
 
     printf("Nome da sua Cidade: ");
     scanf("%s", &cidade);
 
     printf("Quantidade da populção da sua Cidade: ");
-    scanf("%d", &populacão);
-
+    scanf("%f", &populacao); //Em caso de valores com mais de um ponto "." digitar somente um (Ex: 4.234.234 = 4.234234).
+    
     printf("Área total da sua Cidade: ");
-    scanf("%f", &area);
+    scanf("%f", &area);//Em caso de valores com mais de um ponto "." digitar somente um (Ex: 4.234.234 = 4.234234).
 
     printf("Pib da sua Cidade: ");
-    scanf("%f", &pib);
+    scanf("%f", &pib);//Em caso de valores com mais de um ponto "." digitar somente um (Ex: 4.234.234 = 4.234234).
 
     printf("Quantidade de pontos Turísticos: ");
-    scanf("%d", &turistico);
+    scanf("%d", &turistico);//Digitar valores sem ponto ou virgula.
     
-    printf("Inicial: %c" , estado);
-    
+    //Saida de dados no terminal, após Preenchimento.
+    //Os dados preenchidos antes agora irão sair organizados e formarão os dados da carta.
+    //Printf: Responsável por adiministrar a saída dos dados no terminal, antes preenchidos.
 
-    return 0;
+    printf("Carta: 1");
+    printf("Estado: %c\n" , estado);
+    printf("Codígo: %s\n" , codigocarta);
+    printf("Nome da Cidade: %s\n" , cidade);
+    printf("População: %f\n" , populacao);
+    printf("Área: %.3f\n" , area); //Utilização de ".3" para especificar as casa decimais depois do ponto.
+    printf("PIB: %.3f\n" , pib); //Utilização de ".3" para especificar as casa decimais depois do ponto.
+    printf("Número de Pontos Turísticos: %d\n" , turistico);
+
+    return 0; //Finalização do Progarma. 
 }
